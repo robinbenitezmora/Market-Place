bf5d1a2b021df07fb0c0c1d18431ec9663f9b040
robinbenitezmora <robinbenitez1990@gmail.com>
2023-03-14 14:54:31 -0500

    Create the 'User' model and its migration to database

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  namespece :api, defaults: { format::json } do
-    namespece :v1 do
+  namespace :api do
+    namespace :v1 do
     #We are going to list our resources here
     end
   end
