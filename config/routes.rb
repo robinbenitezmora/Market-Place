a8fbb261d31d4a35f6a2876afc3512c874f095ef
robinbenitezmora <robinbenitez1990@gmail.com>
2023-03-23 20:26:08 -0500

    Setup tokens controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      get 'tokens/create'
       resources :users, only: %i[show create update destroy]
+      resources :tokens, only: [:create]
     end
   end
 end
