494aed5d8bc91f8bd518cba3c88475d5cecb2284
robinbenitezmora <robinbenitez1990@gmail.com>
2023-03-24 22:14:20 -0500

    Add destroy products method

--- test/controllers/api/v1/products_controller_test.rb
+++ test/controllers/api/v1/products_controller_test.rb
@@ -52,4 +52,22 @@ class Api::V1::ProductsControllerTest < ActionDispatch::IntegrationTest
       as: :json
     assert_response :forbidden
   end
+
+  test "should destroy product" do
+    assert_difference('Product.count', -1) do
+      delete api_v1_product_url(@product),
+        headers: { Authorization: JsonWebToken.encode(user_id: @product.user_id) },
+        as: :json
+    end
+    assert_response :no_content
+  end
+
+  test "should forbid destroy product" do
+    assert_no_difference('Product.count') do
+      delete api_v1_product_url(@product),
+        headers: { Authorization: JsonWebToken.encode(user_id: users(:two).id)},
+        as: :json
+    end
+    assert_response :forbidden
+  end
 end
