76b8221ce56c1b39511de0f8e84298d7a7fe77a1
robinbenitezmora <robinbenitez1990@gmail.com>
2023-03-27 16:48:46 -0500

    Add search class method to filter products

--- app/models/product.rb
+++ app/models/product.rb
@@ -15,4 +15,19 @@ class Product < ApplicationRecord
   scope :recent, lambda {
     order(:updated_at)
   }
+
+  def self.filter_by_title(keyword)
+    where('lower(title) LIKE ?', "%#{keyword.downcase}%")
+  end
+
+  def self.search(params = {})
+    products = params[:product_ids].present? ? Product.where(id: params[:product_ids]) : Product.all
+
+    products = products.filter_by_title(params[:keyword]) if params[:keyword]
+    products = products.above_or_equal_to_price(params[:min_price]) if params[:min_price]
+    products = products.below_or_equal_to_price(params[:max_price]) if params[:max_price]      
+    products = products.recent if params[:recent]
+      
+    products
+  end
 end
