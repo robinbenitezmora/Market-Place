fd21c03a070499bad8102e83d22849114e0fa759
robinbenitezmora <robinbenitez1990@gmail.com>
2023-03-27 16:25:11 -0500

    Add search scopes on the product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,16 @@ class Product < ApplicationRecord
 
   validates :title, :user_id, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0 }, presence: true
+
+  scope :above_or_equal_to_price, lambda { |price|
+    where('price >= ?', price)
+  }
+
+  scope :below_or_equal_to_price, lambda { |price|
+    where('price <= ?', price)
+  }
+
+  scope :recent, lambda {
+    order(:updated_at)
+  }
 end
