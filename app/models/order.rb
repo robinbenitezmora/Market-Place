fe4ed36e681f000ce04268648663b497e2079543
robinbenitezmora <robinbenitez1990@gmail.com>
2023-03-28 15:48:46 -0500

    Add the create method for the orders controller

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,13 @@
 class Order < ApplicationRecord
+  before_validation :set_total!
+  
   has_many :placements, dependent: :destroy
   has_many :products, through: :placements
   belongs_to :user
   validates :total, numericality: { greater_than_or_equal_to: 0 }
   validates :total, presence: true
+
+  def set_total!
+    self.total = products.map(&:price).sum
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :orders, only: %i[index show]
+      resources :orders, only: %i[index show create]
       resources :users, only: %i[show create update destroy]
       resources :tokens, only: [:create]
       resources :products
